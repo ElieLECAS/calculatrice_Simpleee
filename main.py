0b47c91d72238164ec44e700e1a7c1831a1a476a
Elie LECAS <elielecas00@gmail.com>
2023-10-11 12:09:24 +0200

    12.09

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-print(addition(nombre1,nombre2))
-print(soustraction(nombre1, nombre2))
-print(division(nombre1,nombre2))
 
 def addition(nombre1, nombre2):
     return nombre1 + nombre2
-print(addition(1,3))
+
 
 def soustraction(nombre1, nombre2):
     resultat = nombre1 - nombre2
     return resultat
+
+def division(nombre1, nombre2):
+    return nombre1 / nombre2
+
+
+print(addition(1,9))
+print(soustraction(456, 2))
+print(division(9,2))
